a15fccbc3967d2fc58499eec99f5ab460788466d
Germán Escobar <german.escobarc@gmail.com>
2022-01-25 15:16:51 -0500

    Add linter error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
 
 const Home = React.lazy(() => import("./pages/Home"))
 
